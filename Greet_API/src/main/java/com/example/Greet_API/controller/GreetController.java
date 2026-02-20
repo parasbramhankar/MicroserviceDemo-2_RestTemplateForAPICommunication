087c2d18e01e59ab6579a410c909b1b2e8c47d10
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-20 22:45:34 +0530

    Commit

---
+++ Greet_API/src/main/java/com/example/Greet_API/controller/GreetController.java
@@ -0,0 +1,17 @@
+package com.example.Greet_API.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class GreetController {
+
+    @GetMapping("/greet")
+    public String greet(){
+        return "Good Morning,";
+    }
+
+
+}
