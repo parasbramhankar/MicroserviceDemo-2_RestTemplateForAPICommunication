e90143e2d2d6e7e212918809f989c7b2de4151c0
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-20 22:51:51 +0530

    commit

--- Welcome-API/src/main/java/com/example/Welcome_API/WelcomeApiApplication.java
+++ Welcome-API/src/main/java/com/example/Welcome_API/WelcomeApiApplication.java
@@ -2,8 +2,10 @@ package com.example.Welcome_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WelcomeApiApplication {
 
 	public static void main(String[] args) {
