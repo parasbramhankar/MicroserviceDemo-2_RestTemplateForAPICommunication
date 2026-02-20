e615ca35b2964664af9593e6fba21e1f042d7eba
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-20 22:51:49 +0530

    commit

---
+++ Welcome-API/src/main/java/com/example/Welcome_API/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.example.Welcome_API.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+
+    }
+}
