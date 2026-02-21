086e5b5f2c1417deac95d7895223f1530b8c5e07
parasbramhankar <parasbramhankar.in@gmail.com>
2026-02-21 22:03:35 +0530

    Commit

--- Greet_API/src/main/java/com/example/Greet_API/controller/GreetController.java
+++ Greet_API/src/main/java/com/example/Greet_API/controller/GreetController.java
@@ -1,7 +1,5 @@
 package com.example.Greet_API.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class GreetController {
 
-
-
     @GetMapping("/greet")
     public String greet(){
-
-
         return "Good Morning,";
     }
 
--- Welcome-API/src/main/java/com/example/Welcome_API/controller/WelcomeController.java
+++ Welcome-API/src/main/java/com/example/Welcome_API/controller/WelcomeController.java
@@ -1,15 +1,21 @@
 package com.example.Welcome_API.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/welcome")
 public class WelcomeController {
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/welcome")
     public String welcome(){
-        return "paras";
+
+        String forObj=restTemplate.getForObject("http://localhost:8081/api/greet",String.class);
+
+        return forObj+" paras";
     }
 }
